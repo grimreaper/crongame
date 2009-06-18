7791a243567ef47a93ecc847bb11a82e29e362cc
eitan.adler@gmail.com <eitan.adler@gmail.com@47d41842-5b99-11de-b3c1-0fd540ce45e4>
2009-06-18 00:23:28 +0000

    Let the brick do more work; NEXT STEP: figure out why the heck we move through bricks

--- include/brick.h
+++ include/brick.h
@@ -48,6 +48,12 @@ class Brick
 		void kill_brick(); //set life to 0
 		void set_life(int toWhat); //set life equal to some number
 		int get_life();
+		// While we could always find out what the current status is 
+		// of the brick - the question is do we want the status to 
+		// cause any change. This function should only return a status
+		// when its ready to let the user activate it.
+		// (As of now when life <=1)
+		power::brickStatus getUsableStatus();
 
 	private:
 		void make_brick(int level);

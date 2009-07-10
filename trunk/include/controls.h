e467cf3ffc02a7db3859e2d2c27d0e9335c70767
eitan.adler@gmail.com <eitan.adler@gmail.com@47d41842-5b99-11de-b3c1-0fd540ce45e4>
2009-07-10 03:32:41 +0000

    lets start work on the controls class

---
+++ trunk/include/controls.h
@@ -0,0 +1,15 @@
+/*
+	This class contains a series of functions that return true or false
+	based on if certain controls are pressed or not pressed. This allowed for future user config controls
+	as well as mouse+ketboard+joystick controls.
+*/
+#ifndef CONTROL_H
+#define CONTROL_H
+
+class controls
+{
+	public:
+		bool release_ball_from_paddle();
+};
+
+#endif

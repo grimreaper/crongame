84f4af9ed04dfe4226549466b08b16a48e04a44a
eitan.adler@gmail.com <eitan.adler@gmail.com@47d41842-5b99-11de-b3c1-0fd540ce45e4>
2009-07-10 03:38:14 +0000

    ball release still works; 1 step foward towards customizabability of the controls

--- src/controls.cpp
+++ src/controls.cpp
@@ -1,5 +1,7 @@
-#include "controls.h";
+#include "controls.h"
 
 bool controls::release_ball_from_paddle()
 {
+	/* If the left button is pressed or or if the space bar is pressed. */
+	return ((mouse_b & 1) || key[KEY_SPACE]);
 }

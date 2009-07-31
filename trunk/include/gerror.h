68b61f19a8959987d58fcffe3e5ae721499825c6
eitan.adler@gmail.com <eitan.adler@gmail.com@47d41842-5b99-11de-b3c1-0fd540ce45e4>
2009-07-31 03:49:31 +0000

    small change

--- trunk/include/gerror.h
+++ trunk/include/gerror.h
@@ -1,12 +1,12 @@
 #ifndef GERROR_H
 #define GERROR_H
 /*! \file gerror.h
-Error message system.
+\brief Error message system.
 */
 
 #include "common.h"
 
-/// Goes back to text mode, shows the message and ends the program. Printf style.
+/*! Goes back to text mode, shows the message and ends the program. Printf style. */
 void raise_error(AL_CONST char *msg, ...); 
 
 #ifdef TEST
